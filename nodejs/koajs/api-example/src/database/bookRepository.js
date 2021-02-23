bdaf98f4522924ba05ec5706b684db4b410d3346
Thomas <anhnt@mageplaza.com>
2021-02-23 23:39:33 +0700

    Use the file driver for database

--- nodejs/koajs/api-example/src/database/bookRepository.js
+++ nodejs/koajs/api-example/src/database/bookRepository.js
@@ -1,16 +1,12 @@
-const books = [
-  {id: 101, name: 'Fight Club', author: 'Chuck Palahniuk'},
-  {id: 102, name: 'Sharp Objects', author: 'Gillian Flynn'},
-  {id: 103, name: 'Frankenstein', author: 'Mary Shelley'},
-  {id: 104, name: 'Into The Willd', author: 'Jon Krakauer'}
-];
+const {data: books} = require('./books.json');
+
 
 /**
  *
  * @returns {[{author: string, name: string, id: number}, {author: string, name: string, id: number}, {author: string, name: string, id: number}, {author: string, name: string, id: number}]}
  */
 function getAll() {
-  return books;
+  return books
 }
 
 /**
