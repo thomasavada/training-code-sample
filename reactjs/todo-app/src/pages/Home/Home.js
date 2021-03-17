a99159b175eb4673a942a168e5a62a9a07a3c6f7
tuananh <anhnt@mageplaza.com>
2021-03-17 09:46:16 +0700

    Update the position input

--- reactjs/todo-app/src/pages/Home/Home.js
+++ reactjs/todo-app/src/pages/Home/Home.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-import {CalloutCard, Page} from "@shopify/polaris";
+import {CalloutCard, Page, Card, Layout, FormLayout} from "@shopify/polaris";
+import DesktopPositionInput from "../../components/DesktopPositionInput/DesktopPositionInput";
 
 const Home = props => {
-  return (
-    <Page title={'Home'}>
-      <CalloutCard
-        title="Welcome to AVADA Development"
-        illustration="https://cdn.shopify.com/s/assets/admin/checkout/settings-customizecart-705f57c725ac05be5a34ec20c05b94298cb8afd10aac7bd9c7ad02030f48cfa0.svg"
-        primaryAction={{
-          content: 'Go to Todoes',
-          url: '/todoes',
-        }}
-      >
-        <p>Upload your store’s logo, change colors and fonts, and more.</p>
-      </CalloutCard>
-    </Page>
-  );
+    const [position, setPosition] = useState('top-left')
+    return (
+        <Page title={'Home'}>
+            <Layout>
+                <Layout.Section>
+                    <CalloutCard
+                        title="Welcome to AVADA Development"
+                        illustration="https://cdn.shopify.com/s/assets/admin/checkout/settings-customizecart-705f57c725ac05be5a34ec20c05b94298cb8afd10aac7bd9c7ad02030f48cfa0.svg"
+                        primaryAction={{
+                            content: 'Go to Todoes',
+                            url: '/todoes',
+                        }}
+                    >
+                        <p>Upload your store’s logo, change colors and fonts, and more.</p>
+                    </CalloutCard>
+                </Layout.Section>
+                <Layout.Section>
+                    <Card>
+                        <Card.Section>
+                            <FormLayout>
+                                <DesktopPositionInput
+                                    label="Desktop Position"
+                                    onChange={(val) => setPosition(val)}
+                                    value={position}
+                                />
+                            </FormLayout>
+                        </Card.Section>
+                    </Card>
+                </Layout.Section>
+            </Layout>
+        </Page>
+    );
 };
 
-Home.propTypes = {
-
-};
+Home.propTypes = {};
 
 export default Home;
